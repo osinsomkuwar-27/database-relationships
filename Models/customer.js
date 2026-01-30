323ec0520d6a8deb4a8a53510d21df912715a8c9
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2026-01-30 20:17:40 +0530

    One to Many which stores a reference to the child document inside parent

---
+++ Models/customer.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main()
+  .then(() => console.log("Connection successful"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+const orderSchema = new Schema({
+  item : String,
+  price : Number,
+});
+
+const customerSchema = new Schema({
+  name : String,
+  orders : [{
+    type : mongoose.Schema.Types.ObjectId,
+    ref : "Order"
+  },],
+});
+
+const Order = mongoose.model("Order", orderSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomers = async() =>{
+    // let cust1 = new Customer({name: "Alice"}
+    // );
+
+    // let order1 = await Order.findOne({item: "Laptop"});
+    // let order2 = await Order.findOne({item: "Phone"});
+
+    // cust1.orders.push(order1);
+    // cust1.orders.push(order2);
+
+    // let res = await cust1.save();
+    // console.log("Customer saved:", res);
+
+    let result = await Customer.findOne({});
+    console.log("Customer fetched:", result);
+};
+
+addCustomers();
+
+// const addOrders = async() =>{
+//     let res = await Order.insertMany([
+//         {item: "Laptop", price: 1200},
+//         {item: "Phone", price: 800},
+//         {item: "Tablet", price: 600}
+//     ]);
+//     console.log("Orders added:", res);
+// };
+
+// addOrders();
